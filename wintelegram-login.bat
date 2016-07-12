ae278b9500f136802a4c4b1a4ebe3f8c32a9a21d
Diego Maia <diegosantosmia@gmail.com>
2016-07-12 14:07:31 -0300

    VERSAO 1.0

---
+++ wintelegram-login.bat
@@ -0,0 +1,33 @@
+@ echo off
+
+rem ##########################################################################
+rem # Windows envio de alerta de login no Telegram
+rem # Author: Diego Maia - diegosmaia@yahoo.com.br Telegram - @diegosmaia
+rem ##########################################################################
+
+SET CURL=c:\temp\curl.exe
+
+SET USUARIO=%USERNAME%
+SET SERVIDOR=%ComputerName%
+
+rem # O ID DO SEU USUARIO NO TELEGRAM
+SET USER=150000000
+
+
+rem ############################################
+rem # O Bot-Token o codigo enviado pelo BotFather
+rem ############################################
+
+SET BOT_TOKEN=161080402:AAGah3HIxM9jUr0NX1WmEKX3cJCv9PyWD58
+
+
+rem ############################################
+rem # Envio Mensagem de Texto do Alerta
+rem ############################################
+rem echo " %CURL% -k -X GET https://api.telegram.org/bot%BOT_TOKEN%/sendMessage?chat_id=%USER%&text=O usuario %USUARIO% realizou login no servidor 
+%SERVIDOR%"
+  
+%CURL% -k  -X GET "https://api.telegram.org/bot%BOT_TOKEN%/sendMessage?chat_id=%USER%&text=O usuario %USUARIO%@%USERDOMAIN% realizou login no servidor 
+%SERVIDOR%  Data:%date% %time% "
+
+
