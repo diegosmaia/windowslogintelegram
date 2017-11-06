fd00e644a331a994b05934fcf668d211f3ec830d
Diego Maia <diegosantosmaia@gmail.com>
2017-11-06 07:25:22 -0200

    Ver 2.0 Add IP RDP na MSG

--- wintelegram-login.bat
+++ wintelegram-login.bat
@@ -9,6 +9,9 @@ SET CURL=c:\temp\curl.exe
 
 SET USUARIO=%USERNAME%
 SET SERVIDOR=%ComputerName%
+netstat -na | find "3389" | find "ESTABLISHED" > RDPTMP
+SET /p RDP= < RDPTMP
+DEL RDPTMP
 
 rem # O ID DO SEU USUARIO NO TELEGRAM
 SET USER=150000000
@@ -24,10 +27,5 @@ SET BOT_TOKEN=161080402:AAGah3HIxM9jUr0NX1WmEKX3cJCv9PyWD58
 rem ############################################
 rem # Envio Mensagem de Texto do Alerta
 rem ############################################
-rem echo " %CURL% -k -X GET https://api.telegram.org/bot%BOT_TOKEN%/sendMessage?chat_id=%USER%&text=O usuario %USUARIO% realizou login no servidor 
-%SERVIDOR%"
   
-%CURL% -k  -X GET "https://api.telegram.org/bot%BOT_TOKEN%/sendMessage?chat_id=%USER%&text=O usuario %USUARIO%@%USERDOMAIN% realizou login no servidor 
-%SERVIDOR%  Data:%date% %time% "
-
-
+%CURL% -k  -X GET "https://api.telegram.org/bot%BOT_TOKEN%/sendMessage?chat_id=%USER%&text=O usuario %USUARIO%@%USERDOMAIN% realizou login no servidor %SERVIDOR% - %RDP% - Data:%date% %time% "
